c0575e1ec4a52ac66058e879b56a5472fa435aeb
Andrewphanguyen <97638780+Andrewphanguyen@users.noreply.github.com>
2022-02-09 16:03:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,6 +1,6 @@
 git clone $1
 #mkdir markdown-parse/
-cp GradingTests.java ~/https://github.com/Andrewphanguyen/CSE15L-Panther.git #markdown-parse/
+cp GradingTests.java ~/grade-markdown-parse/CSE15L-Panther #markdown-parse/
 #cd markdown-parse
 cd ~/grade-markdown-parse/CSE15L-Panther
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
